a8e16d37e3934065f7bc267fca2017a90d16e39f
= <njokuchukwuma48@gmail.com>
2024-09-02 19:39:12 +0100

    another commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Join from "./Join";
+import { Buffer } from "buffer";
+window.Buffer = Buffer;
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import process from "process";
+window.process = process;
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <div>Hello logins</div>,
+    errorElement: <h1>Error could not locate resource </h1>,
+  },
+  {
+    path: "/register",
+    element: <div>Hello register</div>,
+    errorElement: <h1>Sorry resource not found</h1>,
+  },
+  {
+    path: "/",
+    element: <div>Hello home page</div>,
+  },
+  {
+    path: "/app",
+    element: <App />,
+  },
+  {
+    path: "/creator/join",
+    element: <Join />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // This is necessary for some wallet libraries
+      buffer: "buffer",
+      process: "process/browser",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfilling plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+});
