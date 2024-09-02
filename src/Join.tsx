6dd92a91a680796d06eda0b403f7fd8ced21c42a
Arthakker <arthuronyeanusi@gmail.com>
2024-09-02 21:13:19 +0100

    worked on creator/join

--- src/Join.tsx
+++ src/Join.tsx
@@ -2,6 +2,12 @@ import Web3 from "web3";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import content from './assets/content.png'
+import communities from './assets/communities.png'
+import live from './assets/live-streaming.png'
+import creative from './assets/creative.png'
+import idea from './assets/idea.png'
+import network from './assets/network.png'
 
 const providerOptions = {
   walletconnect: {
@@ -53,18 +59,28 @@ const Join = () => {
   };
   return (
     <div className="h-screen max-h-screen w-full bg-gradient-to-tr from-slate-950 to-blue-950 flex justify-center items-center">
-      <section className="rounded-2xl border border-blue-900 shadow-xl shadow-blue-700 h-[30rem] w-[30rem] flex flex-col justify-center items-center p-10">
-        {/* <div className="flex space-x-3">
-          <label className="text-white text-2xl">UserName</label>
-          <input type="text" className="text-2xl outline-none border-none" />
-        </div> */}
+      <section className="rounded-2xl z-20 bg-white/70 backdrop-blur-sm shadow-md shadow-blue-700 h-[30rem] w-[30rem] items-center p-10 ">
+      <h1 className="text-slate-900 font-bold text-3xl font-mono mb-14 text-center">Welcome Creator!</h1>
+        <img src={content} alt="" width={100} height={100} className="bg-slate-900 filter invert sepia saturate-200 hue-rotate-90 rounded-full object-cover flex mx-auto"/>
+          <input type="text" className="text-lg mt-10 outline-none flex border-blue-950 w-full text-center rounded-xl border px-3 py-1 font-bold" placeholder="username"/>
+  
         <button
           onClick={handleConnectWallet}
-          className="bg-gradient-to-b from-orange-800 to-orange-700 rounded-2xl  px-7 py-3 text-white text-center"
+          className="bg-gradient-to-b from-orange-800 to-orange-700 rounded-2xl  px-7 py-3 my-5 mx-auto flex text-white text-center"
         >
           Connect Wallet
         </button>
       </section>
+      <img src={content} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 top-20 left-20" width={100} height={100}/>
+      <img src={idea} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 top-40 left-60" width={100} height={100}/>
+      <img src={communities} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 top-80 left-20" width={100} height={100}/>
+      <img src={live} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 bottom-10 left-80" width={100} height={100}/>
+      {/* <img src={content} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 bottom-20 right-20" width={100} height={100}/> */}
+      <img src={creative} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 top-60 right-20" width={100} height={100}/>
+      <img src={live} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 top-10 right-10" width={100} height={100}/>
+      <img src={network} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 top-80 right-80" width={100} height={100}/>
+      <img src={creative} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 bottom-10 right-10" width={100} height={100}/>
+      <img src={idea} alt="" className="absolute filter invert sepia saturate-200 opacity-50 hue-rotate-90 top-0 left-1/2" width={100} height={100}/>
     </div>
   );
 };
