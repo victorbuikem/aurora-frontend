d408d18ec6c77d154f2deb2fe13381bec46eca62
victorbuikem <chibuikemkalu466@gmail.com>
2024-09-03 13:35:52 +0100

    test push

--- src/components/connect-btn.tsx
+++ src/components/connect-btn.tsx
@@ -8,7 +8,7 @@ export function ConnectBtn() {
         <button
             onClick={async () => await connectWallet()}
             className="
-  inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-fuchsia-400 h-10 px-4 py-2 mx-auto w-full mt-10 "
+  inline-flex items-center justify-center whitespace-nowrap text-white rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-black h-10 px-4 py-2 mx-auto w-full mt-10 "
         >
             Connect Wallet
         </button>
--- src/main.tsx
+++ src/main.tsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "/{}",
+    path: "/:id",
     element: <Arthakker1324 />,
   },
 ]);
